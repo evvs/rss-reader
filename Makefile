59eeb9aac12fa3c6bbaeb04097ae727a6420fca5
Evgeniy <va.evgenii@gmail.com>
2020-05-27 20:46:16 +0300

    init

---
+++ Makefile
@@ -0,0 +1,4 @@
+webpack dev:
+	npx webpack --mode development
+webpack prod:
+	npx webpack --mode production
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: ["@babel/polyfill", "./src/app.js"],
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
+  module: {
+    rules: [
+      { test: /\.js$/, 
+        exclude: /node_modules/, 
+        loader: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env"
+            ]
+          }
+        } }
+    ]
+  }
+};
